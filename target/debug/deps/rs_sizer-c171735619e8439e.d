fe618019365bf8f51e19a51bcfb1b0cde6e683e0
Alexey Ermolaev <alexeyermolaev91@gmail.com>
2022-01-26 21:41:25 +0200

    rm files

--- target/debug/deps/rs_sizer-c171735619e8439e.d
+++
@@ -1,5 +0,0 @@
-/home/abc/Documents/rs_sizer/target/debug/deps/rs_sizer-c171735619e8439e: src/main.rs
-
-/home/abc/Documents/rs_sizer/target/debug/deps/rs_sizer-c171735619e8439e.d: src/main.rs
-
-src/main.rs:
--- target/debug/rs_sizer.d
+++
@@ -1,1 +0,0 @@
-/home/abc/Documents/rs_sizer/target/debug/rs_sizer: /home/abc/Documents/rs_sizer/src/main.rs
