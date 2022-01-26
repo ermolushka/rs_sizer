e685b1e50658896419df34668585133e58b9e191
Alexey Ermolaev <alexeyermolaev91@gmail.com>
2022-01-26 22:21:11 +0200

    added sort and debug messages

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ use std::collections::HashMap;
 
 fn main() {
     let mut map: HashMap<String, f64> = HashMap::new();
-    get_entries(Path::new("/home/abc/Downloads"), &mut map);
-    println!("{:?}", map);
+    get_entries(Path::new("/home/abc/Downloads/test_images"), &mut map);
+    let mut count_vec: Vec<_> = map.iter().collect();
+    let sorted_vec: Vec<(&String, &f64)> = sort_metadata(count_vec);
+    println!("{:?}", sorted_vec);
+    print_vec(sorted_vec);
 
 }
 
@@ -30,4 +33,19 @@ fn get_entries(dir: &Path, map: &mut HashMap<String, f64>) -> io::Result<()> {
 
 fn bytes_to_megabyte(bytes: u64) -> f64 {
     bytes as f64 * 0.000001 as f64
+}
+
+fn sort_metadata<'a>(mut vec: Vec<(&'a String, &'a f64)>) -> Vec<(&'a String, &'a f64)> {
+    vec.sort_by(|(k1, _), (k2, _)| k2.cmp(k1));
+    vec.reverse();
+    vec
+
+}
+
+fn print_vec<'a>(mut vec: Vec<(&'a String, &'a f64)>) -> () {
+    let mut counter: i32 = 1;
+    for entry in &vec {
+        println!("line {}, file: {}, size (mb): {}", counter, entry.0, entry.1);
+        counter += 1;
+    }
 }
