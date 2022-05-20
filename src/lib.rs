e51c3e8843597a9e689c65bc24e23ba5015cb4fb
ermolushka <alexeyermolaev91@gmail.com>
2022-05-20 19:05:03 +0200

    rm one flag

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,6 @@ use std::collections::HashMap;
 
 pub fn get_info(path: &String, num_of_items: i32, level: &String, mut map: HashMap<String, f64>) {
     match level.as_str() {
-        "folder" => {
-            println!("folder");
-
-            let mut count_vec: Vec<_> = map.iter().collect();
-            let sorted_vec: Vec<(&String, &f64)> = sort_metadata(count_vec, num_of_items);
-            print_vec(sorted_vec);
-
-        },
         "file" => {
             println!("file");
             get_folders(Path::new(path), &mut map);
